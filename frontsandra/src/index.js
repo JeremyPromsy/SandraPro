209e1ce2bcf36089b944f8fbc28518c9876c058d
JeremyPromsy <jeremypromsy@gmail.com>
2023-11-24 16:47:09 +0100

    Début

---
+++ frontsandra/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+
+import Home from './pages/Home'
+import Error404 from './pages/404'
+import Contact from './pages/Contact'
+import Mentions from './pages/MentionsLégales'
+import Informations from './pages/Informations'
+
+import "./index.css"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+    <Nav />
+    <Routes>
+      <Route path = "/" element ={<Home />} />
+      <Route path = "/contact" element ={<Contact />} />
+      <Route path = "/mentionslégales" element ={<Mentions />} /> 
+      <Route path = "/informations" element ={<Informations />} /> 
+      <Route path = "*" element ={<Error404 />} /> 
+    </Routes>
+    <Footer />
+    </HashRouter>
+  </React.StrictMode>,
+);
+
