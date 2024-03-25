ebc930aa23967f023f300bb15e6004a2ffcc75ab
JeremyPromsy <jeremypromsy@gmail.com>
2024-03-25 16:17:11 +0100

    Avancement

--- frontsandra/src/pages/Financier.jsx
+++ frontsandra/src/pages/Financier.jsx
@@ -1,2 +1,18 @@
 import React from "react";
 import "../styles/financier.css"
+
+function Financier() {
+    return (
+      <div className="pageInfomations">
+        <h1>Mon histoire</h1>
+        <div className="parcours">
+            <h2>Mon parcours</h2>
+        </div>
+        <div className="diplome">
+            <h2>Mes diplômes</h2>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Financier;
