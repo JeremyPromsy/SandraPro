041f199f79e07961952ef9ecded8cc56f78792ee
JeremyPromsy <jeremypromsy@gmail.com>
2024-03-25 15:33:53 +0100

    Mise en place des pages

--- frontsandra/src/pages/Home.jsx
+++ frontsandra/src/pages/Home.jsx
@@ -5,15 +5,27 @@ function Home () {
     return (
         <div className="home">
             <h1>Découvrez VPA : Votre patrimoine autrement</h1>
-            <div className="presentation">
-            <h2>Mes préstations</h2>
-           </div>
-           <div className="quiSuisJe">
-           <h2>Qui suis-je ?</h2>
-           </div>
-           <div className="contactHome">
-           <h2>Contactez-moi !</h2>
-           </div>
+                <div className="presentation">
+                    <h2>Mes préstations</h2>
+                    <div className="gestion">
+                        <h3>Gestion</h3>
+                    </div>
+                    <div className="financier">
+                        <h3>Finacier</h3>
+                    </div>
+                    <div className="immobilier">
+                        <h3>Immobilier</h3>
+                    </div>
+                    <div className="art">
+                        <h3>Art-thérapie</h3>
+                    </div>
+                </div>
+                <div className="quiSuisJe">
+                    <h2>Qui suis-je ?</h2>
+                </div>
+                <div className="contactHome">
+                    <h2>Contact</h2>
+                </div>
         </div>
     )
 }
