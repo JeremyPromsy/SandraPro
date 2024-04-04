963aa5aa767b9709ed8fb6507219f61b9d0b27a4
JeremyPromsy <jeremypromsy@gmail.com>
2024-04-04 16:26:14 +0200

    Changements

--- frontsandra/src/pages/Contact.jsx
+++ frontsandra/src/pages/Contact.jsx
@@ -10,9 +10,9 @@ function Contact() {
         <h1>Contactez-nous !</h1>
         <FormContact />
         <div className="boutonMail">
-          <p>Ou envoyez-nous un mail directement, <br></br>si vous le souhaitez, en cliquant sur le bouton ci-dessous.</p>
+          <p>Ou envoyez-nous un mail directement, en cliquant sur le bouton ci-dessous.</p>
           <div className="boutonMailContact">
-            <a href="mailto:lafamicale@gmail.com"  aria-label="contacter l'agence par mail"><button className="buttonContact">Me joindre </button></a>
+            <a href="mailto:lafamicale@gmail.com"  aria-label="contacter l'agence par mail"><button className="buttonContactPage">Me joindre </button></a>
           </div>
         </div>
       </div>
