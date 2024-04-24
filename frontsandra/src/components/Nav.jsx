30d0b681e6241f31730d1d7702540c76a7afc777
JeremyPromsy <jeremypromsy@gmail.com>
2024-04-24 17:30:18 +0200

    header en attente du logo final

--- frontsandra/src/components/Nav.jsx
+++ frontsandra/src/components/Nav.jsx
@@ -5,35 +5,43 @@ import logoNav from "../assets/VPA-Logo.png";
 
 function Navbar() {
   return (
-      <div className="navLogo">
-        <div className="logoNav">
-            <NavLink to="/">
-                    <img className="logo-img" src={logoNav} alt="logo VPA" />
-            </NavLink>
-        <div className="sidebar-links">
-          <ul className="classNav">
-            <li className="nav-links nav-links1">
-              <NavLink to="/Gestion">Gestion</NavLink>
-            </li>
-            <li className="nav-links nav-links2">
-              <NavLink to="/financier">Financier</NavLink>
-            </li>
-            <li className="nav-links nav-links3">
-              <NavLink to="/immobilier">Immobilier</NavLink>
-            </li>
-            <li className="nav-links nav-links4">
-              <NavLink to="/art-thérapie">Art-Thérapie</NavLink>
-            </li>
-            <li className="nav-links nav-links5">
-              <NavLink to="/informations">Qui suis-je ?</NavLink>
-            </li>
-            <li className="nav-links nav-links6">
+    <nav className="header">
+      <div className="navHeader">
+        <div className="navLogo">
+          <div className="navGauche">
+            <div className="logoNav">
+              <NavLink to="/">
+                <img className="logo-img" src={logoNav} alt="logo VPA" />
+              </NavLink>
+            </div>
+            <div className="sidebar-links">
+              <ul className="classNav">
+                <li className="nav-links nav-links1">
+                  <NavLink to="/Gestion">Gestion</NavLink>
+                </li>
+                <li className="nav-links nav-links2">
+                  <NavLink to="/financier">Financier</NavLink>
+                </li>
+                <li className="nav-links nav-links3">
+                  <NavLink to="/immobilier">Immobilier</NavLink>
+                </li>
+                <li className="nav-links nav-links4">
+                  <NavLink to="/art-thérapie">Art-Thérapie</NavLink>
+                </li>
+                <li className="nav-links nav-links5">
+                  <NavLink to="/informations">Me connaitre</NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="contactNav">
+            <button className="nav-links nav-links6">
               <NavLink to="/contact">Contact</NavLink>
-            </li>
-          </ul>
+            </button>
+          </div>
         </div>
-        </div>
-        </div>
+      </div>
+    </nav>
   );
 }
 
