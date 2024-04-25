7f061130b1c38bdfd43697bbd5ae8b3201f5ab77
JeremyPromsy <jeremypromsy@gmail.com>
2024-04-25 16:53:22 +0200

    Avancement du site

--- frontsandra/src/components/Footer.jsx
+++ frontsandra/src/components/Footer.jsx
@@ -7,13 +7,52 @@ import logo from "../assets/VPA-Logo.png"
 function Footer() {
     return (
         <div className="footer">
+            <div className="footerHaut">
+            <div className="footerGauche">
                 <NavLink to="/">
-                    <img className="logoFooter" 
-                    src= {logo} 
-                    alt ="logo LaFamicale" /> 
+                    <img className="logoFooter" src= {logo} alt ="logo LaFamicale" /> 
                 </NavLink>
-            <div className="nav-linksFooter">
-                <Link to="/mentionslégales">Mentions Légales</Link>
+            </div>
+            <div className="footerMilieu">
+                <ul className="navLinksFooterMid"> 
+                    Pages
+                    <li className="nav-links-footer nav-links-footer1">
+                    <NavLink to="/Gestion">Gestion</NavLink>
+                    </li>
+                    <li className="nav-links-footer nav-links-footer2">
+                    <NavLink to="/financier">Financier</NavLink>
+                    </li>
+                    <li className="nav-links-footer nav-links-footer3">
+                    <NavLink to="/immobilier">Immobilier</NavLink>
+                    </li>
+                    <li className="nav-links-footer nav-links-footer4">
+                    <NavLink to="/art-thérapie">Art-Thérapie</NavLink>
+                    </li>
+                    <li className="nav-links-footer nav-links-footer5">
+                    <NavLink to="/informations">Me connaitre</NavLink>
+                    </li>
+                </ul>
+            </div>
+            <div className="footerDroit">
+                <div className="nav-linksFooter">
+                    <div className="footerBoutonContact">
+                        <button className="nav-links-footer nav-links-footer6">
+                            <NavLink to="/contact">Contact</NavLink>
+                        </button>
+                    </div>
+                    <div className="footerBoutonLinkedin">
+                        <button className="nav-links-footer nav-links-footer7">
+                            <NavLink to="/contact">Contact</NavLink>
+                        </button>
+                    </div>
+                    <div className="footerMentions">
+                        <Link to="/mentionslégales">Mentions Légales</Link>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div className="footerBas">
+                VPA by Sandra - © 2024 Tous droits réservés
             </div>
         </div>
     )
